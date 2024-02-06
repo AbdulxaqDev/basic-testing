dd0386550007cc3896d9752952504c3b5c0253c2
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-06 18:53:55 +0300

    init: test-class and complete 6 subtests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,29 +1,40 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount } from '.';
 
 describe('BankAccount', () => {
+  const initialBalance = 100;
+  const firstAccount = getBankAccount(initialBalance)
+  const secondAccound = getBankAccount(initialBalance)
+  const firstBalance = firstAccount.getBalance()
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(firstAccount.getBalance()).toBe(initialBalance)
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => firstAccount.withdraw(firstBalance + 5)).toThrow(`Insufficient funds: cannot withdraw more than ${firstBalance}`)
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    expect(() => firstAccount.transfer(firstBalance + 5, secondAccound)).toThrow(`Insufficient funds: cannot withdraw more than ${firstBalance}`)
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    expect(() => firstAccount.transfer(firstBalance, firstAccount)).toThrow(`Transfer failed`)
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const depositMoney = 77;
+    const oldBalance = firstAccount.getBalance();
+    firstAccount.deposit(depositMoney)
+    expect(firstAccount.getBalance()).toBe(oldBalance + depositMoney)
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const withdrawMoney = 77;
+    const oldBalance = firstAccount.getBalance();
+    const withdrawAccount = firstAccount.withdraw(withdrawMoney)
+    expect(withdrawAccount.getBalance()).toBe(oldBalance - withdrawMoney)
   });
 
   test('should transfer money', () => {
